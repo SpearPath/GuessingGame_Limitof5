88aa6901eec8ee6b87c9543ec30a0b44909a46de
Sean Darwin Ollanas <seandarwin13@gmail.com>
2025-04-21 10:41:55 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,10 @@
-﻿using GuessingGame;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace GuessingGameWH
+namespace GuessingGameL5
 {
     internal static class Program
     {
