1fa46a6fe6b362c542372a0c6c2a20f388a88843
SpearPath <seandarwin13@gmail.com>
2025-04-21 10:13:36 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace GuessingGame
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.lblHint = new System.Windows.Forms.Label();
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.lblGuessCount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblHint
+            // 
+            this.lblHint.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblHint.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHint.Location = new System.Drawing.Point(25, 30);
+            this.lblHint.Name = "lblHint";
+            this.lblHint.Size = new System.Drawing.Size(350, 40);
+            this.lblHint.TabIndex = 0;
+            this.lblHint.Text = "Guess a number between 0 and 100!";
+            this.lblHint.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txtGuess.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGuess.Location = new System.Drawing.Point(125, 90);
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.Size = new System.Drawing.Size(150, 34);
+            this.txtGuess.TabIndex = 1;
+            this.txtGuess.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtGuess.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtGuess_KeyPress);
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnGuess.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuess.Location = new System.Drawing.Point(125, 140);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(150, 40);
+            this.btnGuess.TabIndex = 2;
+            this.btnGuess.Text = "Guess";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnNewGame.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNewGame.Location = new System.Drawing.Point(25, 220);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(350, 50);
+            this.btnNewGame.TabIndex = 3;
+            this.btnNewGame.Text = "New Game";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Visible = false;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // lblGuessCount
+            // 
+            this.lblGuessCount.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblGuessCount.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGuessCount.Location = new System.Drawing.Point(25, 190);
+            this.lblGuessCount.Name = "lblGuessCount";
+            this.lblGuessCount.Size = new System.Drawing.Size(350, 30);
+            this.lblGuessCount.TabIndex = 4;
+            this.lblGuessCount.Text = "Guesses: 0/5";
+            this.lblGuessCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.ClientSize = new System.Drawing.Size(400, 300);
+            this.Controls.Add(this.lblGuessCount);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.txtGuess);
+            this.Controls.Add(this.lblHint);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Number Guessing Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHint;
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.Label lblGuessCount;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace GuessingGame
+{
+    public partial class Form1 : Form
+    {
+        // Game variables
+        private int targetNumber;
+        private int guessCount;
+        private readonly int maxGuesses = 5;
+        private readonly Color higherColor = Color.LightCoral;
+        private readonly Color lowerColor = Color.LightBlue;
+        private readonly Color neutralColor = SystemColors.Control;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            Random r = new Random();
+            targetNumber = r.Next(0, 101); // Random number 0-100
+            guessCount = 0;
+
+            // Reset UI
+            lblHint.Text = "Guess a number between 0 and 100!";
+            lblHint.TextAlign = ContentAlignment.MiddleCenter;
+            lblGuessCount.Text = $"Guesses: {guessCount}/{maxGuesses}";
+            this.BackColor = neutralColor;
+            txtGuess.Text = "";
+            btnGuess.Enabled = true;
+            btnNewGame.Visible = false;
+            txtGuess.Focus();
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtGuess.Text, out int userGuess))
+            {
+                guessCount++;
+                lblGuessCount.Text = $"Guesses: {guessCount}/{maxGuesses}";
+
+                if (userGuess == targetNumber)
+                {
+                    lblHint.Text = $"Correct! You guessed it in {guessCount} tries!";
+                    this.BackColor = Color.LightGreen;
+                    btnGuess.Enabled = false;
+                    btnNewGame.Visible = true;
+                }
+                else if (guessCount >= maxGuesses)
+                {
+                    lblHint.Text = $"Game over! The number was {targetNumber}.";
+                    this.BackColor = Color.LightGray;
+                    btnGuess.Enabled = false;
+                    btnNewGame.Visible = true;
+                }
+                else if (userGuess < targetNumber)
+                {
+                    lblHint.Text = "Higher! Try again.";
+                    this.BackColor = lowerColor;
+                }
+                else
+                {
+                    lblHint.Text = "Lower! Try again.";
+                    this.BackColor = higherColor;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid number between 0 and 100.", "Invalid Input");
+            }
+
+            txtGuess.Text = "";
+            txtGuess.Focus();
+        }
+
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            InitializeGame();
+        }
+
+        private void txtGuess_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
